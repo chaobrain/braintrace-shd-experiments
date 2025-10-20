bb071debb5d12193bc1d0d35c8bbc903c33df939
Chaoming Wang <adaduo@outlook.com>
2025-10-20 13:08:13 +0800

    update copyright information to reflect BrainX Ecosystem Limited

--- batchnorm.py
+++ batchnorm.py
@@ -1,4 +1,4 @@
-# Copyright 2025 BDP Ecosystem Limited. All Rights Reserved.
+# Copyright 2025 BrainX Ecosystem Limited. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 
 from typing import Tuple, Optional
 
--- online_vmap_models.py
+++ online_vmap_models.py
@@ -1,16 +1,18 @@
+# Copyright 2025 BrainX Ecosystem Limited. All Rights Reserved.
 #
-# SPDX-FileCopyrightText: Copyright © 2022 Idiap Research Institute <contact@idiap.ch>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-FileContributor: Alexandre Bittar <abittar@idiap.ch>
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# This file is part of the sparch package
-#
-"""
-This is to define the experiment class used to perform training and testing
-of ANNs and SNNs on all speech command recognition datasets.
-"""
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 
 import datetime
 import errno
